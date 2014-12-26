1483a34d4d8a7403a54df4c48f07374b4022bd2e
Arthur Sonzogni <Arthur.Sonzogni@ensimag.grenoble-inp.fr>
2014-12-26 14:00:27 +0100

    add the utility : glError

---
+++ src/utils/glError.hpp
@@ -0,0 +1,19 @@
+/**
+ * glError.hpp
+ * Contributors:
+ *      * Arthur Sonzogni (author)
+ * Licence:
+ *      * Public Domain
+ */
+
+#ifndef GLERROR_J4YD6FVF
+#define GLERROR_J4YD6FVF
+
+// Ask Opengl for errors:
+// Result is printed on the standard output
+// usage :
+//      glCheckError(__FILE__,__LINE__);
+void glCheckError(const char* file, unsigned int line);
+
+#endif /* end of include guard: GLERROR_J4YD6FVF */
+
